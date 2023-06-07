51e8f2737dd1de0a39c18d0372aa8b0c8c32373f
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-06-07 16:04:03 -0700

    Fixed the form for user input.

---
+++ client/src/screens/AddDramas.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import { Header } from "../components/Header";
+
+export const AddDramas = () => {
+  const [dramaName, setDramaName] = useState("");
+  const [genre, setGenre] = useState([]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await Axios.post("http://localhost:5000/dramas", {
+        name: dramaName,
+        genre: genre,
+      });
+      alert('Success!');
+    } catch (error) {
+      console.log(error);
+    }
+
+    // reset the states
+    setDramaName('');
+    setGenre([]);
+  };
+
+  return (
+    <div>
+      <Header />
+
+      <section className="container d-flex justify-content-center align-items-center flex-column m-3 p-5">
+        <h1>Add Dramas</h1>
+        <form className="card p-5 mt-2" onSubmit={onSubmit}>
+          <div className="form-floating mb-3">
+            <input type="text" className="form-control" id="name" value={dramaName} onChange={e => setDramaName(e.target.value)}/>
+            <label htmlFor="name">Name</label>
+          </div>
+          <div className="form-floating mb-3">
+            <textarea
+              name="genre"
+              id="genre"
+              className="form-control"
+              cols="30"
+              rows="5"
+              value={genre}
+              onChange={e => setGenre(e.target.value.split(','))}
+            ></textarea>    
+            <label htmlFor="genre">Genres</label>
+          </div>
+          <button type="submit" className="btn btn-dark btn-block">
+            Add
+          </button>
+        </form>
+      </section>
+    </div>
+  );
+};
