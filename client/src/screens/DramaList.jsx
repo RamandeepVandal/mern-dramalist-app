6e4a22dbecf15379cbb026ff61ac06b1340e9b5b
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-06-07 16:04:17 -0700

    Initial commit.

---
+++ client/src/screens/DramaList.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { Header } from "../components/Header";
+import { Dramas } from "../components/Dramas";
+
+export const DramaList = () => {
+  // dramas
+  const [dramas, setDramas] = useState([{}]);
+
+  // get the data
+  useEffect(() => {
+    const getDramas = async () => {
+      const res = await fetch("http://localhost:5000/dramas");
+      const data = await res.json();
+      setDramas(data);
+    };
+
+    getDramas();
+  }, []);
+
+  // filter drama on delete
+  const filterDrama = (id) => {
+    setDramas(dramas => dramas.filter(drama => drama._id !== id));
+  }
+
+  return (
+    <div>
+      <Header />
+      <Dramas dramas={dramas} filterDrama={filterDrama} />
+    </div>
+  );
+};
