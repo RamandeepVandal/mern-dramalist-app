cabb129ea4816ba80cc164cc56630698d179c958
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-07-12 17:42:41 -0700

    Updated the api call.

--- client/src/screens/FindDramas.jsx
+++ client/src/screens/FindDramas.jsx
@@ -4,8 +4,9 @@ import { Footer } from "../components/Footer";
 
 export const FindDramas = () => {
   // genre and year state
-  const [genre, setGenre] = useState("Romance");
-  const [yearDate, setYearDate] = useState("2023");
+  const [genre, setGenre] = useState(10759);
+  const [page, setPage] = useState(1);
+  const [lang, setLang] = useState("ko");
 
   // dramas
   const [drama, setDrama] = useState([{}]);
@@ -24,10 +25,11 @@ export const FindDramas = () => {
   };
 
   // GET the dramas
-  const getDrama = async (yearDate, genre) => {
+  const getDrama = async (genre, page, lang) => {
     // fetch the data
     const response = await fetch(
-      `https://api.themoviedb.org/3/discover/tv?first_air_date_year=${yearDate}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=horror&with_original_language=ko`,
+      //&first_air_date_year=${yearDate}
+      `https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=${page}&sort_by=popularity.desc&with_genres=${genre}&with_original_language=${lang}&without_genres=16,99,10764,10763,10767,10762`,
       options
     );
     const data = await response.json();
@@ -38,9 +40,11 @@ export const FindDramas = () => {
   const onSubmit = (e) => {
     e.preventDefault();
 
-    const newGenre = genre?.replaceAll(',', '%2C%20');
-    getDrama(yearDate, newGenre )
-  }
+    getDrama(genre, page, lang);
+  };
+
+  // add drama to list
+  
 
   return (
     <section>
@@ -54,24 +58,50 @@ export const FindDramas = () => {
             <div className="col-lg-12 col-md-12 col-sm-12 col-12">
               <form onSubmit={onSubmit} className="card form-card p-5 mt-2">
                 <div className="form-floating mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
+                  <select
+                    class="form-select mb-3"
                     id="genre"
                     value={genre}
                     onChange={(e) => setGenre(e.target.value)}
-                  />
+                  >
+                    <option value="10759">Action & Adventure</option>
+                    <option value="10749">Romance</option>
+                    <option value="35">Comedy</option>
+                    <option value="80">Crime</option>
+                    <option value="18">Drama</option>
+                    <option value="10751">Family</option>
+                    <option value="9648">Mystery</option>
+                    <option value="10765">Sci-Fi & Fantasy</option>
+                    <option value="10766">Soap</option>
+                  </select>
                   <label htmlFor="genre">Genres</label>
                 </div>
                 <div className="form-floating mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="year"
-                    value={yearDate}
-                    onChange={(e) => setYearDate(e.target.value)}
-                  />
-                  <label htmlFor="year">Year</label>
+                  <select
+                    class="form-select mb-3"
+                    id="page"
+                    value={page}
+                    onChange={(e) => setPage(e.target.value)}
+                  >
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                    <option value="4">Four</option>
+                    <option value="5">Five</option>
+                  </select>
+                  <label htmlFor="page">Page</label>
+                </div>
+                <div className="form-floating mb-3">
+                  <select
+                    class="form-select mb-3"
+                    id="lang"
+                    value={lang}
+                    onChange={(e) => setLang(e.target.value)}
+                  >
+                    <option value="ko">Korean</option>
+                    <option value="en">English</option>
+                  </select>
+                  <label htmlFor="lang">Language</label>
                 </div>
                 <button type="submit" className="btn btn-explore">
                   Find
@@ -85,11 +115,15 @@ export const FindDramas = () => {
         <div className="d-flex flex-wrap justify-content-center align-items-center">
           <div className="container">
             <div className="row">
+              {console.log(drama)}
               {drama &&
                 drama.map((item, key) => {
                   return (
                     <div className="col-lg-6 col-md-12 col-sm-12 col-12 d-flex align-items-stretch">
-                      <div className="card find-card p-5 m-5 text-center" key={key}>
+                      <div
+                        className="card find-card p-5 m-5 text-center"
+                        key={key}
+                      >
                         <img
                           src={`http://image.tmdb.org/t/p/w500/${item?.poster_path}`}
                           alt="poster"
@@ -97,6 +131,8 @@ export const FindDramas = () => {
                         />
                         <p className="fs-4">{item?.name}</p>
                         <p>{item?.overview}</p>
+
+                        <button className="btn btn-explore">Add to list</button>
                       </div>
                     </div>
                   );
@@ -113,11 +149,16 @@ export const FindDramas = () => {
 };
 
 /*
-{drama.length > 0 ? drama.results.map((dramaItem, key) => {
-            return (
-              <div className="card" key={key}>
-                <h1>{dramaItem?.name}</h1>
-              </div>
-            );
-          }) : null}
-*/
+
+              <div className="form-floating mb-3">
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="year"
+                    value={yearDate}
+                    onChange={(e) => setYearDate(e.target.value)}
+                  />
+                  <label htmlFor="year">Year</label>
+                </div>
+
+ */
