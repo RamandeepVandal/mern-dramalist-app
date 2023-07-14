f48038fd1a58612fdc7f256189ec7ec79fa6cbd3
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-07-14 16:59:35 -0700

    Removed paths to add and edit drama pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import screens
 import { DramaList } from "./screens/DramaList";
-import { AddDramas } from "./screens/AddDramas";
-import { EditDramas } from "./screens/EditDramas";
 import { LandingPage } from "./screens/LandingPage";
 import { FindDramas } from "./screens/FindDramas";
+import { SearchDramas } from "./screens/SearchDramas";
 
 function App() {
   return (
@@ -13,9 +12,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route exact path="/drama" element={<DramaList />} />
-        <Route exact path="/add" element={<AddDramas />} />
-        <Route exact path="/edit" element={<EditDramas />} />
-        <Route exact path='/find' element={<FindDramas />} />
+        <Route exact path="/find" element={<FindDramas />} />
+        <Route exact path="/search" element={<SearchDramas />} />
       </Routes>
     </Router>
   );
