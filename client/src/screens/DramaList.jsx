a55dae6840d22c8fd0805f303d209751cec43438
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-10-11 21:55:22 -0700

    Added loading screen.

--- client/src/screens/DramaList.jsx
+++ client/src/screens/DramaList.jsx
@@ -2,25 +2,38 @@ import React, { useState, useEffect } from "react";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { Dramas } from "../components/Dramas";
+// bootstrap spinner
+import Spinner from "react-bootstrap/Spinner";
 
 export const DramaList = () => {
   // dramas
-  const [dramas, setDramas] = useState([{
-    name: 'One Spring Night',
-    description: 'When Lee Jeong-in and Yu Ji-ho meet, something unexpected happens. Or it just may be that spring is in the air -- and anything is possible.',
-    imgURL: '/kFEIbuGVbIDQ5GPOWdw6heZVXKC.jpg'
-  }]);
+  const [dramas, setDramas] = useState([
+    {
+      name: "One Spring Night",
+      description:
+        "When Lee Jeong-in and Yu Ji-ho meet, something unexpected happens. Or it just may be that spring is in the air -- and anything is possible.",
+      imgURL: "/kFEIbuGVbIDQ5GPOWdw6heZVXKC.jpg",
+    },
+  ]);
+  // loading
+  const [loading, setLoading] = useState(false);
 
   // get the data
   useEffect(() => {
-    const getDramas = async () => {
-      const res = await fetch("http://localhost:5000/dramas");
-      const data = await res.json();
-      setDramas(data);
-    };
-
+    setLoading(true);
     getDramas();
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
   }, []);
+
+  // get data
+  const getDramas = async () => {
+    const res = await fetch("http://localhost:5000/dramas");
+    const data = await res.json();
+    setDramas(data);
+  };
 
   // filter drama on delete
   const filterDrama = (id) => {
@@ -29,10 +42,28 @@ export const DramaList = () => {
 
   return (
     <div>
-      <Header />
-      <Dramas dramas={dramas} filterDrama={filterDrama} />
-      {/* FOOTER SECTION */}
-      <Footer />
+      {loading ? (
+        <div>
+          <Header />
+          <div className="App">
+            <Spinner
+              animation="border"
+              role="status"
+              variant="success"
+              size="lg"
+            >
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <Header />
+          <Dramas dramas={dramas} filterDrama={filterDrama} />
+          {/* FOOTER SECTION */}
+          <Footer />
+        </div>
+      )}
     </div>
   );
 };
