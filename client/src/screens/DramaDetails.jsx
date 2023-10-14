380c6a6b4872d86b2128aa978f7ebdd44636c3fe
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-10-13 21:04:53 -0700

    Updated font sizes and image responsiveness

--- client/src/screens/DramaDetails.jsx
+++ client/src/screens/DramaDetails.jsx
@@ -42,7 +42,7 @@ export const DramaDetails = () => {
         </div>
       ) : (
         <section
-        className="details"
+          className="details"
           style={{
             backgroundImage: `linear-gradient(
                 rgba(0, 0, 0, 0.65), 
@@ -53,41 +53,39 @@ export const DramaDetails = () => {
           <Header />
 
           <section className="container">
-            <div className="details-section d-flex justify-content-evenly align-items-start m-5 p-5">
+            <div className="details-section d-flex justify-content-between align-items-start m-5 p-5">
               <img
                 src={`http://image.tmdb.org/t/p/w500/${drama.poster_path}`}
                 alt="poster"
-                className="img-fluid"
+                className="img-fluid details-img"
                 loading="lazy"
               />
-              <div className="d-flex flex-column ms-5 detail-container">
-                <h1 className="content-p-lg">{drama.name}</h1>
-                <div className="d-flex flex-row justify-content-start align-items-center">
+              <div className="d-flex flex-column detail-container">
+                <h1 className="content-p details-text">{drama.name}</h1>
+                <div className="d-flex flex-row align-items-center details-text">
                   <p>
                     {drama.first_air_date} ({drama.origin_country})
                   </p>
-                  <div className="d-flex align-items-center">
-                    <p className="ms-5 content-p">
-                      {drama.vote_average}{" "}
-                      <span>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="20"
-                          height="20"
-                          fill="currentColor"
-                          class="bi bi-star-fill"
-                          viewBox="0 0 21 21"
-                        >
-                          <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                        </svg>
-                      </span>
-                    </p>
-                  </div>
+                  <p className="details-rating content-p-sm">
+                    {drama.vote_average}{" "}
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        class="bi bi-star-fill"
+                        viewBox="0 0 21 21"
+                      >
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg>
+                    </span>
+                  </p>
                 </div>
 
-                <div className="mt-5">
-                  <h2 className="content-h-md">Overview</h2>
-                  <p className="content-p">{drama.overview}</p>
+                <div className="mt-5 details-text">
+                  <h2 className="content-p">Overview</h2>
+                  <p className="content-p-sm">{drama.overview}</p>
                 </div>
 
                 <button className="btn btn-explore btn-lg">Add to list</button>
