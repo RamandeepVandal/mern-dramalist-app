172d4f3893ae28aed14a9f87f7b4e3252f6c897b
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-10-16 15:21:44 -0700

    Initial commit.

---
+++ server/models/userSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    savedDramas: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Dramas'
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
