4b0c7de6c446e601dfaceda59c9bab12aed949e1
Ramandeep Vandal <106114063+RamandeepVandal@users.noreply.github.com>
2023-12-03 23:10:17 -0800

    Added react cookies.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,14 @@
+import React from 'react';
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { useNavigate } from "react-router-dom";
+// react cookie
+import { useCookies } from "react-cookie";
 
 export const Header = () => {
+  const [cookies, setCookies] = useCookies("access_token");
+
   // navigation
   const navigate = useNavigate();
   const handleClick = () => navigate("/");
@@ -12,6 +17,13 @@ export const Header = () => {
   const handleClickSearchDramas = () => navigate("/search");
   const handleClickLogin = () => navigate("/login");
   const handleClickRegister = () => navigate("/register");
+
+  // logout
+  const logout = () => {
+    setCookies("access_token", "");
+    window.localStorage.removeItem("userID");
+    navigate('/login');
+  };
 
   return (
     <Navbar collapseOnSelect expand="lg" variant="light">
@@ -24,38 +36,46 @@ export const Header = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link
-              className="me-5 content-p nav-text"
-              onClick={handleClickDrama}
-            >
-              Dramas
-            </Nav.Link>
-            <Nav.Link
-              className="me-5 content-p nav-text"
-              onClick={handleClickFindDramas}
-            >
-              Find Dramas
-            </Nav.Link>
-            <Nav.Link
-              className="me-5 content-p nav-text"
-              onClick={handleClickSearchDramas}
-            >
-              Search Dramas
-            </Nav.Link>
-            <Nav.Link
-              className="me-5 content-p nav-text"
-              onClick={handleClickLogin}
-            >
-              Login
-            </Nav.Link>
-            <Nav.Link
-              className="me-5 content-p nav-text"
-              onClick={handleClickRegister}
-            >
-              Register
-            </Nav.Link>
-          </Nav>
+          {!cookies.access_token ? (
+            <Nav className="ms-auto">
+             <Nav.Link
+                className="me-5 content-p nav-text"
+                onClick={handleClickLogin}
+              >
+                Login
+              </Nav.Link>
+              <Nav.Link
+                className="me-5 content-p nav-text"
+                onClick={handleClickRegister}
+              >
+                Register
+              </Nav.Link>
+            </Nav>
+          ) : (
+            <Nav className="ms-auto">
+              <Nav.Link
+                className="me-5 content-p nav-text"
+                onClick={handleClickDrama}
+              >
+                Dramas
+              </Nav.Link>
+              <Nav.Link
+                className="me-5 content-p nav-text"
+                onClick={handleClickFindDramas}
+              >
+                Find Dramas
+              </Nav.Link>
+              <Nav.Link
+                className="me-5 content-p nav-text"
+                onClick={handleClickSearchDramas}
+              >
+                Search Dramas
+              </Nav.Link>
+              <button className="btn btn-explore btn-md" onClick={logout}>
+                Logout
+              </button>
+            </Nav>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
